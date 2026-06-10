46df9d5033a3283d082b730c4260a6c51ca19489
Sreesanth-01 <iamshanth7@gmail.com>
2026-06-10 18:30:59 +0530

    Initial commit

---
+++ src/main/java/com/project/careerOs/CareerOsApplication.java
@@ -0,0 +1,13 @@
+package com.project.careerOs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerOsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareerOsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/careerOs/CareerOsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.careerOs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareerOsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
