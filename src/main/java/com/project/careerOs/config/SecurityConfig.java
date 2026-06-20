2dc4b59bc5f8c983cc9060dcee14c37d6ef1f15e
Sreesanth-01 <iamshanth7@gmail.com>
2026-06-20 13:37:19 +0530

    setup: Health controller

--- src/main/java/com/project/careerOs/config/SecurityConfig.java
+++ src/main/java/com/project/careerOs/config/SecurityConfig.java
@@ -15,7 +15,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception{
         http.csrf(csrf -> csrf.disable())
         .authorizeHttpRequests(auth -> auth.requestMatchers("/api/auth/**").permitAll()
-            .anyRequest().authenticated())
+            .anyRequest().permitAll())
         .formLogin(form -> form.disable())
         .httpBasic(Customizer.withDefaults());
 
