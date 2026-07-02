808142bf27c5535f82e09eeee49867d5695df710
Sreesanth-01 <iamshanth7@gmail.com>
2026-07-02 22:09:07 +0530

    feat: JobApplication entity

--- src/main/java/com/project/careerOs/model/JobApplication.java
+++ src/main/java/com/project/careerOs/model/JobApplication.java
@@ -1,26 +1,34 @@
 package com.project.careerOs.model;
 
-import java.util.Date;
+import java.time.LocalDate;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
-@Data
+@Getter
+@Setter
 public class JobApplication {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     private String companyName;
-    private Date appliedDate;
-    private String status; //Change to enum later
+    private String position;
+    private long salary;
+    @Enumerated(EnumType.STRING)
+    private Enum status;
+    private LocalDate appliedDate;
     private String notes;
+
 }
---
+++ src/main/java/com/project/careerOs/model/Status.java
@@ -0,0 +1,13 @@
+package com.project.careerOs.model;
+
+public enum Status {
+    SAVED,
+    APPLIED,
+    ASSESSMENT,
+    INTERVIEW,
+    OFFER_RECIEVED,
+    ACCEPTED,
+    REJECTED,
+    WITHDRAWN,
+    ON_HOLD
+}
