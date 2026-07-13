9dd8b4e556d67534c6865da2b180128b5110204b
Sreesanth-01 <iamshanth7@gmail.com>
2026-07-13 12:43:03 +0530

    refactor: Jwt Authentication filter(authentication details)

--- src/main/java/com/project/careerOs/security/JwtAuthenticationFilter.java
+++ src/main/java/com/project/careerOs/security/JwtAuthenticationFilter.java
@@ -4,8 +4,8 @@ import java.io.IOException;
 import java.util.List;
 
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -38,7 +38,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                     String userEmail = jwtUtil.extractEmail(token);
 
                     UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userEmail, null,List.of());
-
+                    authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
                     SecurityContextHolder.getContext().setAuthentication(authentication);
                 }
                 filterChain.doFilter(request, response);
