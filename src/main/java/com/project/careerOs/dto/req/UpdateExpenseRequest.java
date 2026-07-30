a66fcd4b83044fe5a017748ce83658f80ac0bc86
Sreesanth-01 <iamshanth7@gmail.com>
2026-07-30 08:58:41 +0530

    feat: Update Application -Dto

---
+++ src/main/java/com/project/careerOs/dto/req/UpdateExpenseRequest.java
@@ -0,0 +1,19 @@
+package com.project.careerOs.dto.req;
+
+import java.io.ObjectInputFilter.Status;
+import java.time.LocalDate;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UpdateExpenseRequest {
+    public String companyName;
+    public String jobRole;
+    public long salary;
+    public Status status;
+    public LocalDate appliedDate;
+    public String notes;
+
+}
